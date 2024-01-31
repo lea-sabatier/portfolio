b07adb52f07b9f0db47d264e7b7366fd37dee812
Léa Sabatier <129936937+lea-sabatier@users.noreply.github.com>
2024-01-31 12:00:04 +0100

    Add files via upload

---
+++ Streamlit/tabs/bibliographie.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+def tab():
+    st.markdown("""[ecommerce-nation](https://www.ecommerce-nation.fr/commerce-amerique-latine-tendances-opportunites/)""")
+    st.markdown("""[teamfrance-export](https://www.teamfrance-export.fr/infos-sectorielles/15126/15126-la-start-up-bresilienne-de-commerce-electronique-olist-arrive-au-mexique)""")
+    st.markdown("""[blog : l'e-commerce brésilien à la loupe](https://blog.lengow.com/fr/le-commerce-bresilien-a-la-loupe/)""")
+    st.markdown("""[conversion ; ecoomerce in brazil](https://conversion.ag/fr/blog/ecommerce-in-brazil/)""")
---
+++ Streamlit/tabs/recommandations.py
@@ -0,0 +1,77 @@
+import streamlit as st
+
+def tab():
+    st.subheader("Recommandations pour le Service Client :")
+    st.markdown("""- Récompenses pour les Clients Satisfaits :
+    - Mettez en place un programme de récompenses pour les clients ayant attribué des notes de 5 étoiles.
+    - Offrez des avantages tels que des remises exclusives, des promotions spéciales ou des points de fidélité pour encourager la fidélisation.
+- Analyse Approfondie des Notes de 1 à 3 :
+    - Effectuez une analyse approfondie des commentaires des clients qui ont attribué des scores de 1 à 3.
+    - Identifiez les problèmes spécifiques tels que les délais de livraison, la qualité des produits, etc.
+Utilisez ces informations pour mettre en place des actions correctives ciblées.
+- Encouragement et Récompenses pour les Avis :
+    - Mettez en place un système d'encouragement automatisé pour recueillir des avis après la réception du colis.
+    - Offrez des récompenses telles que des coupons de promotion sur les frais de livraison pour encourager les clients à laisser des avis.
+- Feedback sur le Processus d'Achat :
+    - Sollicitez également les avis des clients sur le processus d'achat global.
+    - Identifiez les points forts et les domaines à améliorer dans le processus d'achat.
+- Optimisation de la Chaîne Logistique :
+    - Analysez les causes des retards dans la livraison (7.62% des commandes).
+    - Optimisez la chaîne logistique en collaboration avec les partenaires logistiques pour réduire les retards.
+- Communication Proactive sur les Livraisons :
+    - Mettez en place un système de communication proactive pour informer les clients en temps réel sur l'état de leurs livraisons.
+    - Anticipez les retards potentiels et informez les clients avec des notifications en temps réel.
+- Évaluation Continue :
+    - Surveillez régulièrement les performances du Service Client à l'aide d'indicateurs clés de performance (KPI).
+    - Ajustez les stratégies en fonction des retours clients et des résultats des analyses.""")
+
+    st.subheader("Recommandations pour Partenaires (acquérir nouveaux partenaires):")
+    st.markdown("""- Campagnes d'Acquisition Ciblées :
+    - Lancer des campagnes d'acquisition ciblées pour attirer de nouveaux vendeurs sur la plateforme.
+    - Benchmark et veille les concurrences pour savoir comment nos concurrents attirent les nouveaux vendeurs
+- Programmes d'Incitation :
+    - Développer des programmes d'incitation attrayants pour les nouveaux vendeurs, offrant des avantages exclusifs pendant la période d'inscription.
+    - Récompenser les vendeurs pour leurs performances initiales.
+- Accompagnement Personnalisé :
+    - Mettre en place un processus d'accompagnement personnalisé pour aider les nouveaux vendeurs à comprendre et à tirer le meilleur parti de la plateforme.
+    - Fournir une assistance proactive pour résoudre les problèmes et répondre aux questions.
+- Témoignages de Succès :
+    - Mettre en avant des témoignages de succès d'autres vendeurs sur la plateforme.
+    - Créer du contenu engageant montrant comment d'autres entrepreneurs ont prospéré grâce à Olist. Interview des tops vendeurs sur plateforme Olist par exemple.
+""")
+    
+    st.subheader("Recommandations pour la Gestion des Vendeurs Existants :")
+    st.markdown("""- Analyse des Performances :
+    - Effectuer une analyse approfondie des performances de chaque vendeur, en identifiant les points forts et les domaines d'amélioration.
+- Formation Continue :
+    - Proposer des sessions de formation continue pour les vendeurs, mettant l'accent sur les meilleures pratiques en matière de vente en ligne, de gestion de stock, et de satisfaction client.
+- Récompenses pour la Performance :
+    - Instaurer des programmes de récompenses pour les vendeurs les plus performants, les encourageant à maintenir un niveau élevé de service et de satisfaction client.
+- Promotion des Bonnes Pratiques :
+    - Encourager les vendeurs à ajouter entre 3 et 4 photos par produit pour optimiser la visibilité et les chances de vente.
+    - Partager des conseils sur la création de descriptions de produits attrayantes.
+""")
+
+    st.subheader("Recommandations pôle finance pour Olist :")
+    st.markdown("""- Exploiter la Croissance Potentielle :
+    - Capitaliser sur la croissance observée entre Octobre 2016 et Octobre 2017 (28.32%) en identifiant les facteurs qui ont contribué à cette progression positive.
+    - Reproduire les stratégies efficaces pour stimuler davantage la croissance du CA.
+- Expansion du Nombre de Vendeurs :
+    - Envisager activement une expansion du nombre de vendeurs sur la plateforme pour soutenir la croissance du CA.
+    - Mettre en œuvre des initiatives d'acquisition de nouveaux vendeurs avec des incitations attractives.
+- Optimisation des Performances des Vendeurs :
+    - Analyser les performances des vendeurs existants pour identifier des opportunités d'optimisation.
+    - Fournir un support personnalisé aux vendeurs et partager les meilleures pratiques pour maximiser leur efficacité.
+- Stratégie de Livraison Gratuite :
+    - Mettre en place la stratégie de livraison gratuite en fonction de la distance et de la valeur du panier pour encourager des achats plus importants.
+    - Calculer précisément les coûts logistiques pour maintenir une rentabilité optimale.
+- Reconsidération de la Stratégie de Coût Fixe :
+    - Évaluer la rentabilité de la stratégie de coût fixe (80 Brésilien/mois) à la lumière des coûts d'acquisition des nouveaux clients.
+    - Si les coûts d'acquisition augmentent, reconsidérer la tarification fixe pour garantir la rentabilité.
+- Diversification des Canaux de Publicité :
+    - Diversifier les canaux de publicité pour réduire la dépendance à des coûts élevés sur des plateformes spécifiques.
+    - Explorer des options publicitaires plus rentables et performantes.
+- Évaluation Continue :
+    - Établir des mécanismes d'évaluation continue pour surveiller l'efficacité des stratégies mises en place.
+    - Ajuster les tactiques en fonction des changements dans le paysage du commerce électronique et des comportements des consommateurs.
+""")
